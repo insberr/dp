17dc210f4c765310b9297f4a37828c26d0a2360b
Jonah Matteson <main@insberr.com>
2023-09-04 11:32:38 -0700

    added menu bar

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,24 @@
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import GearIcon from '@mui/icons-material/Settings';
+
+import { PageToRender, pageToRender } from '../storage/pageToRender';
+
+export default function MenuBar() {
+    return (
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <BottomNavigation
+                showLabels
+                value={pageToRender.value}
+                onChange={(event, newValue) => {
+                    pageToRender.value = newValue;
+                }}
+            >
+                <BottomNavigationAction label="Schedule" icon={<RestoreIcon />} value={PageToRender.Schedule} />
+                <BottomNavigationAction label="Links" icon={<FavoriteIcon />} value={PageToRender.Links} />
+                <BottomNavigationAction label="Settings" icon={<GearIcon />} value={PageToRender.Settings} />
+            </BottomNavigation>
+        </Paper>
+    );
+}
