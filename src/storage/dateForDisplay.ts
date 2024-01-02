653ccdbe128d5c87c964b9f87c1fdf3897549111
Jonah Matteson <main@insberr.com>
2024-01-02 03:01:01 -0800

    remove testing date freeze

--- src/storage/dateForDisplay.ts
+++ src/storage/dateForDisplay.ts
@@ -1,6 +1,6 @@
 import { signal } from '@preact/signals';
 
-export const dateForDisplay = signal<Date>(new Date('January 8, 2024'));
+export const dateForDisplay = signal<Date>(new Date());
 
 export const currentDate = signal<Date>(new Date());
 
